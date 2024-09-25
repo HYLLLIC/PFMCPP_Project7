c7fe2ca69fa4e958bc1363fbf363571f1c817f3e
HYLLLIC <nathan.o.sparks@gmail.com>
2024-09-25 04:42:31 +0000

    P7.1 - Helpful items update.

--- DefensiveItem.cpp
+++ DefensiveItem.cpp
@@ -3,7 +3,5 @@
 
 void DefensiveItem::use(Character* character)
 {
-    //assert(false); 
-    //make your defensive item use the appropriate Utility helper functions
     useDefensiveItem(character, *this);
 }
